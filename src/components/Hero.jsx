ea6a8c4cf07593f345cfd844515b45836a3ae556
Shabnam <shabnam.hoypuc1@gmail.com>
2024-11-21 08:13:09 +0530

    second changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <div className='relative h-[100vh] w-full bg-zinc-900 flex justify-center items-center'
       style={{ backgroundImage: `url('/assets/images/sky-stars(1).png')` }}>
-        
-      <div 
+
+      <div
         className='w-[220px] h-[120px] bg-contain bg-center bg-no-repeat absolute top-[4.5rem]'
         style={{
           backgroundImage: `url('/assets/images/logo.png')`
@@ -13,10 +13,15 @@ const Hero = () => {
       </div>
 
       <div
-        className='w-[260px] h-[260px] overflow-hidden mt-[35px] bg-contain bg-center bg-no-repeat opacity-60 relative z-100'
+        className='w-[270px] h-[270px] overflow-hidden mt-[50px] bg-contain bg-center bg-no-repeat opacity-60 relative z-100'
         style={{
           backgroundImage: `url('/assets/images/asteroid.png')`
         }}>
+      </div>
+
+      <div className="absolute bottom-24 text-center">
+        <p className="gradient-text pt-serif-regular host-grotesk">Unlock the treasures of cosmos</p>
+        <p className="tracking-wider mt-2 text-lg">Your additional text here</p>
       </div>
 
       <div className="absolute bottom-8 flex flex-col items-center text-zinc-500">
@@ -26,7 +31,7 @@ const Hero = () => {
         </div>
       </div>
 
-      
+
 
     </div >
   );
